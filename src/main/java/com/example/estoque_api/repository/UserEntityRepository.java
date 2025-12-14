cfe690e453614bf5e9ae7b1756b20acc0843b845
Dev-lucass <lucasbitencourt959@gmail.com>
2025-12-14 15:34:25 -0300

    feat(UserEntityValidation): add UserEntityValidation and apply in UserEntityService

--- src/main/java/com/example/estoque_api/repository/UserEntityRepository.java
+++ src/main/java/com/example/estoque_api/repository/UserEntityRepository.java
@@ -3,6 +3,10 @@ package com.example.estoque_api.repository;
 import com.example.estoque_api.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserEntityRepository extends JpaRepository<UserEntity, Long> {
+import java.util.Optional;
 
+public interface UserEntityRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByUsername (String username);
+    Optional<UserEntity> findByCpf (String cpf);
+    Optional<UserEntity> findByAddress (String address);
 }
--- src/main/java/com/example/estoque_api/service/UserEntityService.java
+++ src/main/java/com/example/estoque_api/service/UserEntityService.java
@@ -2,22 +2,25 @@ package com.example.estoque_api.service;
 
 import com.example.estoque_api.model.UserEntity;
 import com.example.estoque_api.repository.UserEntityRepository;
+import com.example.estoque_api.validation.UserEntityValidation;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Service
 public class UserEntityService {
 
     private final UserEntityRepository repository;
+    private final UserEntityValidation validation;
 
-    public UserEntityService(UserEntityRepository repository) {
+    public UserEntityService(UserEntityRepository repository, UserEntityValidation validation) {
         this.repository = repository;
+        this.validation = validation;
     }
 
     @Transactional
     public UserEntity save(UserEntity user) {
+        validation.validationUserEntityIsDuplicatedOnCreate(user);
         return repository.save(user);
     }
 
@@ -28,18 +31,19 @@ public class UserEntityService {
     @Transactional
     public UserEntity update(Long id, UserEntity userUpdated) {
 
-        UserEntity userEntityFounded = repository.findById(id).orElseThrow(() -> new RuntimeException("Id user not found ..."));
+        UserEntity userEntityFounded = validation.validationUserEntityIdIsValid(id);
+        validation.validationUserEntityIsDuplicatedOnUpdate(userUpdated);
 
         userEntityFounded.setUsername(userUpdated.getUsername());
         userEntityFounded.setCpf(userUpdated.getCpf());
-        userEntityFounded.setAdress(userUpdated.getAdress());
+        userEntityFounded.setAddress(userUpdated.getAddress());
 
         return userEntityFounded;
     }
 
     @Transactional
     public void deleteById(Long id) {
-        UserEntity userEntityFounded = repository.findById(id).orElseThrow(() -> new RuntimeException("Id user not found ..."));
+        UserEntity userEntityFounded = validation.validationUserEntityIdIsValid(id);
         repository.delete(userEntityFounded);
     }
 
