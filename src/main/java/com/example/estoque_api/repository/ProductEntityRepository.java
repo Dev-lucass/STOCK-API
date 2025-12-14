b3bd16d40d2f11a56068b70078f747015567b7c3
Dev-lucass <lucasbitencourt959@gmail.com>
2025-12-14 15:45:57 -0300

    feat(ProductEntityValidation): add ProductEntityValidation and apply in ProductEntityService

--- src/main/java/com/example/estoque_api/repository/ProductEntityRepository.java
+++ src/main/java/com/example/estoque_api/repository/ProductEntityRepository.java
@@ -3,5 +3,8 @@ package com.example.estoque_api.repository;
 import com.example.estoque_api.model.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProductEntityRepository extends JpaRepository<ProductEntity, Long> {
+    Optional<ProductEntity> findByName (String name);
 }
--- src/main/java/com/example/estoque_api/service/ProductEntityService.java
+++ src/main/java/com/example/estoque_api/service/ProductEntityService.java
@@ -2,6 +2,7 @@ package com.example.estoque_api.service;
 
 import com.example.estoque_api.model.ProductEntity;
 import com.example.estoque_api.repository.ProductEntityRepository;
+import com.example.estoque_api.validation.ProductEntityValidation;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
@@ -11,13 +12,16 @@ import java.util.List;
 public class ProductEntityService {
 
     private final ProductEntityRepository repository;
+    private final ProductEntityValidation validation;
 
-    public ProductEntityService(ProductEntityRepository repository) {
+    public ProductEntityService(ProductEntityRepository repository, ProductEntityValidation validation) {
         this.repository = repository;
+        this.validation = validation;
     }
 
     @Transactional
     public ProductEntity save(ProductEntity product) {
+        validation.validationProductEntityIsDuplicatedOnCreate(product);
         return repository.save(product);
     }
 
@@ -28,7 +32,8 @@ public class ProductEntityService {
     @Transactional
     public ProductEntity update(Long id, ProductEntity productUpdated) {
 
-        ProductEntity productEntityFounded = repository.findById(id).orElseThrow(() -> new RuntimeException("Id product not found ..."));
+        ProductEntity productEntityFounded = validation.validationProductEntityIdIsValid(id);
+        validation.validationProductEntityIsDuplicatedOnUpdate(productUpdated);
 
         productEntityFounded.setName(productUpdated.getName());
         productEntityFounded.setActive(productUpdated.getActive());
@@ -38,7 +43,7 @@ public class ProductEntityService {
 
     @Transactional
     public void deleteById(Long id) {
-        ProductEntity productEntityFounded = repository.findById(id).orElseThrow(() -> new RuntimeException("Id product not found ..."));
+        ProductEntity productEntityFounded = validation.validationProductEntityIdIsValid(id);
         repository.delete(productEntityFounded);
     }
 
