4b070dd6780e6459038aac7a80c254e77f7400b7
Dev-lucass <lucasbitencourt959@gmail.com>
2025-12-14 20:02:42 -0300

    feat(InventoryService): add methods take and return inventory

--- src/main/java/com/example/estoque_api/repository/InventoryEntityRepository.java
+++ src/main/java/com/example/estoque_api/repository/InventoryEntityRepository.java
@@ -7,5 +7,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface InventoryEntityRepository extends JpaRepository<InventoryEntity, Long> {
-    Optional<InventoryEntity> findByProduct (ProductEntity product);
+
+    Optional<InventoryEntity> findByProduct (
+            ProductEntity product
+    );
+
+    Optional<InventoryEntity> findByProduct_Id(
+            Long productId
+    );
 }
