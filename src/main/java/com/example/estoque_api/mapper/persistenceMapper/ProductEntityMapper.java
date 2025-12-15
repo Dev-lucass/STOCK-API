6c3d69842d28a8802ff44e91c2d01f3ebaffe250
Dev-lucass <lucasbitencourt959@gmail.com>
2025-12-15 14:04:20 -0300

    fix(mappers): ignore attributes in entities mappers

--- src/main/java/com/example/estoque_api/mapper/persistenceMapper/ProductEntityMapper.java
+++ src/main/java/com/example/estoque_api/mapper/persistenceMapper/ProductEntityMapper.java
@@ -3,11 +3,13 @@ package com.example.estoque_api.mapper.persistenceMapper;
 import com.example.estoque_api.dto.request.ProductEntityDTO;
 import com.example.estoque_api.model.ProductEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ProductEntityMapper {
 
-    ProductEntity toEntity (ProductEntityDTO dto);
+    @Mapping(target = "id", ignore = true)
+    ProductEntity toEntity(ProductEntityDTO dto);
 
-    ProductEntityDTO toDto (ProductEntity product);
+    ProductEntityDTO toDto(ProductEntity product);
 }
