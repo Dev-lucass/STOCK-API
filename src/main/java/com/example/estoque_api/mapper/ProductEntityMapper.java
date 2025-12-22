e91fbce406d19eddc0887e469559a35c28a92191
Dev-lucass <lucasbitencourt959@gmail.com>
2025-12-21 22:37:39 -0300

    feat: ProductEntityService

--- src/main/java/com/example/estoque_api/mapper/ProductEntityMapper.java
+++ src/main/java/com/example/estoque_api/mapper/ProductEntityMapper.java
@@ -22,4 +22,8 @@ public class ProductEntityMapper {
                 .createdAt(LocalDate.now())
                 .build();
     }
+
+    public void updateEntity(ProductEntity entity, ProductEntityDTO dto) {
+        entity.setName(dto.name());
+    }
 }
