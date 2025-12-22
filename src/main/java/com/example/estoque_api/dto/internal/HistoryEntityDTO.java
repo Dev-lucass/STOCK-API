4c35b274e82cab014cfb34e1dbed5898ac114094
Dev-lucass <lucasbitencourt959@gmail.com>
2025-12-22 10:29:36 -0300

    feat: HistoryEntityService

--- src/main/java/com/example/estoque_api/dto/internal/HistoryEntityDTO.java
+++ src/main/java/com/example/estoque_api/dto/internal/HistoryEntityDTO.java
@@ -3,7 +3,9 @@ package com.example.estoque_api.dto.internal;
 import com.example.estoque_api.enums.InventoryAction;
 import com.example.estoque_api.model.ProductEntity;
 import com.example.estoque_api.model.UserEntity;
+import lombok.Builder;
 
+@Builder
 public record HistoryEntityDTO(UserEntity user,
                                                           ProductEntity product,
                                                           InventoryAction action,
---
+++ src/main/java/com/example/estoque_api/dto/response/entity/HistoryEntityResponseDTO.java
@@ -0,0 +1,16 @@
+package com.example.estoque_api.dto.response.entity;
+
+import com.example.estoque_api.enums.InventoryAction;
+import com.example.estoque_api.model.HistoryId;
+import lombok.Builder;
+import org.springframework.format.annotation.DateTimeFormat;
+import java.time.LocalDate;
+
+@Builder
+public record HistoryEntityResponseDTO(HistoryId id,
+                                       Long userId,
+                                       Long productId,
+                                       InventoryAction action,
+                                       int quantity,
+                                       @DateTimeFormat(pattern = "dd/MM/yyyy")
+                                                                 LocalDate createdAt) {}
